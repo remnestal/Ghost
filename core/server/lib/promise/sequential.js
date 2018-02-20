8f7395f2faeb45c1bd17ee0f6dc12693cd91c71f
Anders Sjöbom <anders.sjobom@live.se>
2018-02-20 10:36:36 +0100

    sequential for mail

---
+++ core/server/lib/promise/sequential.js
@@ -0,0 +1,15 @@
+/**  
+ *   
+ * # Execute a number of tasks with a single argument in sequential order   
+ *   
+ */  
+var Promise = require('bluebird');  
+  
+function pipeline(tasks, options) {  
+    // Iterate through the tasks with options as arg  
+    return Promise.reduce(tasks, function (arg, task) {  
+        return task(options);  
+    }, options);  
+}  
+  
+module.exports = pipeline; 
